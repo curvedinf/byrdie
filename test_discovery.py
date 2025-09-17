1816d2ae7ffb7425bd19f033604fe0357aa2ff3b
Chase Adams <chase.adams@gmail.com>
2025-09-16 21:42:15 -0500

    task(delete_test_discovery_script): Execute the shell command 'rm test_discovery.py' in the current working directory to remove the temporary verification script file used for modular discovery testing.

--- test_discovery.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
-from byrdie.cli import bootstrap_byrdie
-bootstrap_byrdie()
-from django.apps import apps
-app_models = apps.get_models(app_label='app')
-for model in app_models:
-    print(model.__name__)
