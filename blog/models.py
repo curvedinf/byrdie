fcb986cc95c8fb94fe4698866fbbc459f04c56e7
Chase Adams <chase.adams@gmail.com>
2025-09-16 21:41:59 -0500

    task(delete_blog_directory): Execute the shell command 'rm -rf blog' in the current working directory to remove the temporary blog directory and all its contents, including any Python modules and files within it.

--- blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from byrdie.models import Model
-
-
-class BlogPost(Model):
-    title = models.CharField(max_length=200, expose=True)
-
-    class Meta:
-        pass
--- blog/views.py
+++
@@ -1,7 +0,0 @@
-
-from byrdie.api import route
-
-@route('/blog/post')
-def blog_post(request):
-    return {'message': 'Blog post view'}
-
